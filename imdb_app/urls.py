ffa96ee70b33002b2f4815e6437d59a24a4163d3
Prakand <prakandmishra5100@gmail.com>
2023-07-04 10:09:42 +0530

    Some_basic_requests

--- imdb_app/urls.py
+++ imdb_app/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+# from django.urls import path
+# from .views import movie_list,movie_details
 
-urlpatterns = [
-    path('', ),
-]
+# urlpatterns = [
+#     path('list/',movie_list,name='movie-list'),
+#     path('list/<int:pk>',movie_details,name='movie-detail'),
+# ]
